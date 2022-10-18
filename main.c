70d05627c09b4858a7a6c85b03ea0c343a194888
Jonathan Lamoureux <jlamoureux@stetson.edu>
2022-10-18 15:18:11 -0400

    Added key generation

--- main.c
+++ main.c
@@ -4,13 +4,23 @@
 
 int main () {
     // Our cipher parameters
+    int num_bytes = 32; // 256-bit key size
+    size_t i;
     unsigned char *iv;
-    unsigned char *key;
+    unsigned char *key = malloc(num_bytes);
+    
+    // Generate the key
+    for (i = 0; i < num_bytes; i++) {
+         key[i] = rand();
+    }
 
     // File information -- only one for testing right now
     FILE *curr_file;
     char fileName[] = "file_to_encrypt.txt";
-    
+
     // Read the binaries of the current file
     curr_file = fopen(fileName, "rb");
+    // Call encrypt function here
+    fclose(curr_file);
+    return 0;
 }
