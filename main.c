67ed378fcd774da26aaccc5812dbe9b77466716d
Jonathan Lamoureux <jlamoureux@stetson.edu>
2022-10-18 15:07:21 -0400

    Struct changes

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
-typedef struct _cipher_params {
-    unsigned char *iv;
-    unsigned char *key;
-} cipher_params;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main () {
-    cipher_params *params = (cipher_params *)malloc(sizeof(cipher_params));
-    params->key = 1;
-    params->iv = 2;
+    // Our cipher parameters
+    unsigned char *iv;
+    unsigned char *key;
+
+    // File information -- only one for testing right now
+    FILE *curr_file;
+    char fileName[] = "file_to_encrypt.txt";
+    
+    // Read the binaries of the current file
+    curr_file = fopen(fileName, "rb");
 }
